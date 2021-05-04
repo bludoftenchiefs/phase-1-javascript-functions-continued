f5725c1ffc8d5bcd9fedd0d6866e3ae307528bc9
rilith <bludoftenchiefs@protonmail.com>
2021-05-04 05:25:00 -0500

    commit tree b5b71f3c3568e387dea776174160bf6a52ac91c7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(job='go to the office'){
+    return `This Monday, I will ${job}.`;
+}
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective(emphatic='*') {
+    let result = function(otherThing='special') {
+        return `You are ${emphatic}${otherThing}${emphatic}!`;
+    }
+    return result;
+}
+
+
+
+
+// - It should return a function
+//   - This "inner" function should:
+//     - take a single parameter that should default to `"special"`. Name it
+//       however you wish.
+//     - return a `String` of the form "You are ..." where `...` should be the
+//       adjective this function received wrapped in visual flair
+// - It should take as parameter a `String` that will be used to create visual flair
+// - You may call the parameter whatever you like, but its default value should
+//   be `"*"`
+// - Call example: `const encouragingPromptFunction = wrapAdjective("!!!")`
